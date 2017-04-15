11f240dde2e7b0732e54085f3d01d96709ee73d1
ham2qur <ham2qur@yahoo.com>
2017-04-15 16:42:42 +0300

    Sabre Exceptions implementations

---
+++ sabre_dev_studio/sabre_exceptions.py
@@ -0,0 +1,90 @@
+class SabreClientError(Exception):
+    pass
+
+# Authentication requested, but no credentials (client ID, client secret) provided
+class NoCredentialsProvided(SabreClientError):
+    pass
+
+# Did not request token
+class NotAuthorizedError(SabreClientError):
+    pass
+
+class UnsupportedMethodError(SabreClientError):
+    pass
+
+class InvalidInputError(SabreClientError):
+    pass
+
+# Base API Exception
+class SabreDevStudioAPIException(Exception):
+    def __init__(self, e=None):
+        if isinstance(e, dict):
+            message = e.get('message')
+            super(SabreDevStudioAPIException, self).__init__(message)
+
+            self.message = message
+            self.status = e.get('status')
+            self.error_code = e.get('errorCode')
+            self.e_type = e.get('type')
+            self.tstamp = e.get('timeStamp')
+        elif isinstance(e, str):
+            self.message = e
+        else:
+            super(SabreDevStudioAPIException, self).__init__()
+
+    def __unicode__(self):
+        if self.message and self.status:
+            str += 'Message:\t' + self.message + '\n'
+            str += 'Status:\t' + self.status + '\n'
+            str += 'Error Code:\t' + self.error_code + '\n'
+            str += 'Type:\t' + self.type + '\n'
+            str += 'Timestamp:\t' + self.timestamp + '\n'
+            return str
+        elif self.message:
+            return self.message
+        else:
+            return "<" + self.__class__.__name__ + ">"
+
+
+# 400
+class SabreErrorBadRequest(SabreDevStudioAPIException):
+    pass
+
+# 401
+class SabreErrorUnauthorized(SabreDevStudioAPIException):
+    pass
+
+# 403
+class SabreErrorForbidden(SabreDevStudioAPIException):
+    pass
+
+# 404
+class SabreErrorNotFound(SabreDevStudioAPIException):
+    pass
+
+# 404
+class SabreErrorMethodNotAllowed(SabreDevStudioAPIException):
+    pass
+
+# 406
+class SabreErrorNotAcceptable(SabreDevStudioAPIException):
+    pass
+
+# 429
+class SabreErrorRateLimited(SabreDevStudioAPIException):
+    pass
+
+# 500
+class SabreInternalServerError(SabreDevStudioAPIException):
+    pass
+
+# 503
+class SabreErrorServiceUnavailable(SabreDevStudioAPIException):
+    pass
+
+# 504
+class SabreErrorGatewayTimeout(SabreDevStudioAPIException): 
+    pass
+
+class SabreErrorUnauthenticated(SabreDevStudioAPIException):
+    pass
